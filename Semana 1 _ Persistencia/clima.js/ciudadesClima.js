6c0e10cc2b385775a1ea01880e423f27eb38d0a3
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2021-09-03 15:15:04 -0300

    exercise cities

--- Semana 1 _ Persistencia/clima.js/ciudadesClima.js
+++ Semana 1 _ Persistencia/clima.js/ciudadesClima.js
@@ -1,46 +1,34 @@
-const express = require('express');
-const app = express();
-import fetch from 'node-fetch';
+const content = document.getElementById("result");
+
 const APIkey = 'c1513656743583242e0f3d410e330fa7';
 const ciudades = ["Roma", "Paris,", "Tokyo", "Moscu", "Chicago", "Boston", "Madrid", "London", "Lima", "BuenosAires"]
 
-api = 'https://api.openweathermap.org/data/2.5/weather?q='
-
 const cuidadRandom = () => {
-    return ciudades[Math.floor(Math.random()*ciudades.length)];
+  return ciudades[Math.floor(Math.random()*ciudades.length)];
 }
 
 const ciudades3 = [
   {ciudad: cuidadRandom(),
+   temp: "",
   }, 
   {ciudad: cuidadRandom(),
+   temp: "",
   }, 
   {ciudad: cuidadRandom(),
+   temp: "",
   }];
-
-count = 0
-const agregarTemp = (temp) => {
-  
-  ciudades3[count].temperatura = temp
-  count++
-}
 
 async function clima () {
     
-  for(i=0;i<3;i++){
+  for(i=0;i<ciudades3.length;i++){
+
     ciudad = ciudades3[i].ciudad
     ciudadAPI = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${APIkey}`
 
-    const response = await fetch(url);
+    const response = await fetch(ciudadAPI);
     const info = await response.json();
-
-    console.log(info)
-  };
+    if (info.main.temp) ciudades3[i].temp = parseFloat(info.main.temp - 273.15).toFixed(2)
+  }
 }
-  
-clima();
-
-const port = 3000
-
-app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`));
-
+clima ()
+console.log(ciudades3)
