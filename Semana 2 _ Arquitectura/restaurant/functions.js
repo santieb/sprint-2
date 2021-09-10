796b104bbabc7fc657dc6c2351fc740eb1024030
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2021-09-10 20:59:43 -0300

    restaurant

--- Semana 2 _ Arquitectura/restaurant/functions.js
+++ Semana 2 _ Arquitectura/restaurant/functions.js
@@ -1,87 +1,26 @@
 const express = require('express');
 const plate = require('./plateModel');
 
+const listPlates = async () => await plate.find();
 
-const listPlates = async () => {
-    let response;
-    try {
-        response = await plate.find();
-    }catch(err) {
-        response =  err;
-    }
-    console.log(response)
-    return response;
-} 
+const getPlateById = async (dishId) => await plate.findById(dishId);
 
-
-const createPlate = async (body) => {
-    let response;
-    try {
-        const newPlate = await new plate(body);
-        response = await newPlate.save();
-    } catch(err) {
-      response = await err;
-    }
-    console.log(response)
+const createPlate = async (dish) => {
+    const newPlate = new plate(dish);
+    const response = await newPlate.save();
     return response;
 }
+const updatePlate = async (dishId, dish) => await plate.findByIdAndUpdate(dishId, dish);
 
+const deletePlate = async (dishId) => await plate.findByIdAndDelete(dishId);
 
-const updatePlate = async (plateID, body) => {
-    let response;
-    try {
-        response = await plate.findByIdAndUpdate(plateID, body);
-    } catch(err) {
-      response = await err;
-    }
-    console.log(response)
-    return response;
-}
-
-
-const deletePlate = async (plateID) => {
-    let response;
-    try {
-        response = await plate.findByIdAndDelete(plateID);
-    } catch(err) {
-      response = await err;
-    }
-    console.log(response)
-    return response;
-}
-
-const findPlate = async (plateID) => {
-    let response;
-    try {
-        response = await plate.findById(plateID);
-    } catch(err) {
-      response = await err;
-    }
-    console.log(response)
-    return response;
-}
-
-
-const validatePlate = async (req, res, next) => {
-    let response;
-    try {
-        response = await plate.exists({ _id: req.params.id });
-        if(response) next()
-    } catch(err) {
-      response = await err;
-    }
-    console.log(response)
-    res.json(response)
-}
-
-const listDishes = async () => await plate.find();
+const plateExists = async (dishId) => await plate.exists({ _id: dishId });
 
 module.exports = {
     listPlates,
+    getPlateById,
     createPlate,
-    deletePlate,
     updatePlate,
-    findPlate,
-    validatePlate,
-    listDishes
+    deletePlate,
+    plateExists
 }
--- Semana 2 _ Arquitectura/restaurant/plateRoute.js
+++ Semana 2 _ Arquitectura/restaurant/plateRoute.js
@@ -10,30 +10,44 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+const dishExists = (req, res, next) => {
+    functions.plateExists(req.params.id)
+    .then((result) => result ? next() : res.status(404).send('Dish not found'))
+    .catch(() => res.status(404).json('Dish not found'));
+  }
+  
+  app.get('/get', function (req, res) {
+    functions.listPlates()
+    .then(dishes => res.json(dishes))
+    .catch(err => res.json(err));
+  });
+  
+  app.get('/find/:id', dishExists, function (req, res) {
+    functions.getPlateById(req.params.id)
+    .then(dishes => res.json(dishes))
+    .catch(err => res.json(err));
+  });
+  
+  app.post('/post', function (req, res) {
+    functions.createPlate(req.body)
+    .then(dish => res.json(dish))
+    .catch(err => res.json(err));
+  });
+  
+  app.put('/update/:id', dishExists, function (req, res) {
+    functions.updatePlate(req.params.id, req.body)
+    .then(dish => res.json(dish))
+    .catch(err => res.json(err));
+  });
+  
+  app.delete('/delete/:id', dishExists, function (req, res) {
+    functions.deletePlate(req.params.id)
+    .then(dish => res.json(dish))
+    .catch(err => res.json(err));
+  });
+  
 
-app.get('/get', (req, res) => {
-    res.json(functions.listPlates())
-});
 
-
-app.get('/find/:id', functions.validatePlate, (req, res) => {
-    res.json(functions.findPlate(req.params.id))
-});
-
-
-app.post('/post', (req, res) => {
-    res.json(functions.createPlate(req.body))
-});
-
-
-app.put('/update/:id', functions.validatePlate, (req, res) => {
-    res.json(functions.updatePlate(req.params.id, req.body))
-});
-
-
-app.delete('/delete/:id', functions.validatePlate, (req, res) => {
-    res.json(functions.deletePlate(req.params.id))
-});
 
 
 app.listen(port, () => {
