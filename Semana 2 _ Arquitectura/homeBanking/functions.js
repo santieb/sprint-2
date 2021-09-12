e1b049be1a99c501cc066fdd7a3c8b9105c95a03
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2021-09-11 21:21:43 -0300

    refactor code

--- Semana 2 _ Arquitectura/homeBanking/functions.js
+++ Semana 2 _ Arquitectura/homeBanking/functions.js
@@ -1,54 +1,70 @@
-const { response } = require('express');
-const users = require('./userModel');
+const { response } = require("express");
+const users = require("./userModel");
 
 const createUser = async (user) => {
-    const newUser = new users(user);
-    const response = await newUser.save();
-    return response;
-}
+  const newUser = new users(user);
+  const response = await newUser.save();
+  return response;
+};
 
 const modifyBalance = async (req) => {
-    const aux = await users.findOne({ "mail": req.body.mail })
-    const balance = req.body.amount + aux.balance
-    const response = await users.findOneAndUpdate({ "mail": req.body.mail }, { "balance": balance})
-    return response
-}
+  const aux = await users.findOne({ mail: req.body.mail });
+  const balance = req.body.amount + aux.balance;
+  const response = await users.findOneAndUpdate(
+    { mail: req.body.mail },
+    { balance: balance }
+  );
+  return response;
+};
 
 const transfer = async (req) => {
-    const aux = await users.findOne({ "mail": req.body.originMail })
-    const balance = aux.balance - req.body.amount
-    const res1 = await users.findOneAndUpdate({ "mail": req.body.originMail }, { "balance": balance})
+  const aux = await users.findOne({ mail: req.body.originMail });
 
-    const aux2 = await users.findOne({ "mail": req.body.destinationMail })
-    const balance2 = aux2.balance + req.body.amount
-    const res2 = await users.findOneAndUpdate({ "mail": req.body.destinationMail }, { "balance": balance2})
+  //if(aux.balance - req.body.amount < 0) abortar session
 
-    const response = [res1, res2]
-    return response    
-}
+  const balance = aux.balance - req.body.amount;
+  const res1 = await users.findOneAndUpdate(
+    { mail: req.body.originMail },
+    { balance: balance }
+  );
 
+  const aux2 = await users.findOne({ mail: req.body.destinationMail });
+  const balance2 = aux2.balance + req.body.amount;
+  const res2 = await users.findOneAndUpdate(
+    { mail: req.body.destinationMail },
+    { balance: balance2 }
+  );
 
-const findMail = async (mail) => await users.exists({ "mail": mail });
+  const response = [res1, res2];
+  return response;
+};
+
+const findMail = async (mail) => await users.exists({ mail: mail });
 
 const mailExist = (req, res, next) => {
-    findMail(req.body.mail)
-    .then((result) => result ? next() : res.status(404).send('mail doest exist'))
-    .catch(() => res.status(404).json('mail doest exist'));
-}
-  
+  findMail(req.body.mail)
+    .then((result) =>
+      result ? next() : res.status(404).send("mail doest exist")
+    )
+    .catch(() => res.status(404).json("mail doest exist"));
+};
 
-const findMails = async (req) => await findMail(req.body.originMail) && await findMail(req.body.destinationMail);
+const findMails = async (req) =>
+  (await findMail(req.body.originMail)) &&
+  (await findMail(req.body.destinationMail));
 
 const mailsExist = (req, res, next) => {
-    findMails(req)
-    .then((result) => result ? next() : res.status(404).send('mail doest exist'))
-    .catch(() => res.status(404).json('mail doest exist'));
-}
+  findMails(req)
+    .then((result) =>
+      result ? next() : res.status(404).send("mail doest exist")
+    )
+    .catch(() => res.status(404).json("mail doest exist"));
+};
 
 module.exports = {
-    createUser,
-    modifyBalance,
-    transfer,
-    mailExist,
-    mailsExist
-}
+  createUser,
+  modifyBalance,
+  transfer,
+  mailExist,
+  mailsExist,
+};
--- Semana 2 _ Arquitectura/homeBanking/index.js
+++ Semana 2 _ Arquitectura/homeBanking/index.js
@@ -1,45 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3024;
-const host = 'http://localhost';
-const functions = require('./functions')
-const users = require('./userModel');
-
+const host = "http://localhost";
+const functions = require("./functions");
+const users = require("./userModel");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.post("/", (req, res) => {
+  let user = {
+    name: req.body.name,
+    lastname: req.body.lastname,
+    mail: req.body.mail,
+    balance: req.body.balance,
+  };
 
-app.post('/', (req, res) => {
-
-    let user = {
-        name: req.body.name,
-        lastname: req.body.lastname,
-        mail: req.body.mail,
-        balance: req.body.balance
-    }
-
-    functions.createUser(user)
-    .then(response => res.json(response))
-    .catch(err => res.json(err));
+  functions
+    .createUser(user)
+    .then((response) => res.json(response))
+    .catch((err) => res.json(err));
 });
-  
-app.put('/balance', functions.mailExist, (req, res) => {
 
-    functions.modifyBalance(req)
-    .then(response => res.json(response))
-    .catch(err => res.json(err));
+app.put("/balance", functions.mailExist, (req, res) => {
+  functions
+    .modifyBalance(req)
+    .then((response) => res.json(response))
+    .catch((err) => res.json(err));
+});
 
-})
-
-app.put('/transfer', functions.mailsExist, (req, res) => {
-
-    functions.transfer(req)
-    .then(response => res.json(response))
-    .catch(err => res.json(err));
-
-})
-
-
+app.put("/transfer", functions.mailsExist, (req, res) => {
+  functions
+    .transfer(req)
+    .then((response) => res.json(response))
+    .catch((err) => res.json(err));
+});
 
 app.listen(port, () => console.log(`${host}:${port}`));
