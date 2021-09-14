f86420accace10418d440e047a7f6e1a0506bdf7
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2021-09-13 22:52:05 -0300

    challenge 2 transaction

--- Semana 2 _ Arquitectura/challenge2.js
+++ Semana 2 _ Arquitectura/challenge2.js
@@ -1,45 +1,39 @@
-const { response } = require('express');
-const mongoose = require('mongoose');
-const { schema } = require('./restaurant/plateModel');
-const plate = require('./restaurant/plateModel');
+const mongoose = require("mongoose");
 
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
 const conectionString = `mongodb://localhost:27017`;
 mongoose.connect(conectionString, options);
 
-
 user = {
-    name: String,
-    money: Number
-}
+  name: String,
+  money: Number,
+};
 
-const users = mongoose.model('Users', user);
+const Users2 = mongoose.model("Users2", user);
 
-const juan = {name: 'Juan', money: 100};
+const juan = { name: "Juan", money: 100 };
 
+let newUser = new Users2(juan);
+newUser.save();
 
-const createUser = async () => {
-
+const list = async () => {
+  let response;
+  try {
     const session = await mongoose.startSession();
-    session.startTransaction()
+    session.startTransaction();
 
+    response = await Users2.findOne({ name: "Jua" });
+    await session.commitTransaction();
+    session.endSession();
+  } catch (err) {
+    response = err;
+  }
+  console.log(response);
+  return response;
+};
+list();
 
-
-    try {
-        const newUser = await new users(juan);
-        response = await newUser.save().session(session)
-        assert.ok(!response)
-    }catch(err) {
-        response =  err;
-    }
-    return response;
-} 
-
-
-
-
-list()
