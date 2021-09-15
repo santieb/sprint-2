ad40acd2eda796b7fd8994d7d03d4195d0d543b4
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2021-09-14 21:06:00 -0300

    challenge 1

--- Semana 3 _ DB relacional/challenge1.sql
+++ Semana 3 _ DB relacional/challenge1.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 15-09-2021 a las 02:02:25
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
