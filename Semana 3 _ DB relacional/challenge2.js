b657bde251e8b5e8348b3fcad8c6c925537031b0
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2021-09-16 12:40:01 -0300

    exercise bands

--- Semana 3 _ DB relacional/challenge2.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize ('mysql://root@localhost:3306')
-
-sequelize.query('SELECT * FROM `users`',
-{ type: sequelize.QueryTypes.SELECT }
-).then(function(resultados) {
-    console.log(resultados)
-});
